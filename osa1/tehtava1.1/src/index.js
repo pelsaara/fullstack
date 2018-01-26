6ede0e9c4b1bda2575d7f0c66986f36b15cce4be
pelsaara <saara.peltomaki@gmail.com>
2018-01-26 14:46:55 +0200

    tehtava 1.5 tehty

--- osa1/tehtava1.1/src/index.js
+++ osa1/tehtava1.1/src/index.js
@@ -12,9 +12,9 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa osa={props.osat[0]}/>
-            <Osa osa={props.osat[1]}/>
-            <Osa osa={props.osat[2]}/>
+            <Osa osa={props.osat[0]} />
+            <Osa osa={props.osat[1]} />
+            <Osa osa={props.osat[2]} />
         </div>
     )
 }
@@ -37,28 +37,30 @@ const Yhteensa = (props) => {
 
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osat = [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
-    ]
-  
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14
+            }
+        ]
+    }
+
 
     return (
         <div>
-            <Otsikko kurssi={kurssi} />
-            <Sisalto osat={osat}/>
-            <Yhteensa osat={osat}/>
+            <Otsikko kurssi={kurssi.nimi} />
+            <Sisalto osat={kurssi.osat} />
+            <Yhteensa osat={kurssi.osat} />
         </div>
     )
 }

9b9f34cb8459dc9f5f150f00df35504340faf796
pelsaara <saara.peltomaki@gmail.com>
2018-02-01 15:50:33 +0200

    tehtava 2.4-2.5 tehty

--- osa2/tehtava2.1/src/index.js
+++ osa2/tehtava2.1/src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 
-//const Yhteensa = (props) => {
-//  const [osa1, osa2, osa3] = props.kurssi.osat
-  
-//  return(
-//    <p>yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
-//  )
-//}
+
 
 
 

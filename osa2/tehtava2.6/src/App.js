ada20b58bc5ff0604570fccc562d43d1b38397c1
pelsaara <saara.peltomaki@gmail.com>
2018-02-02 18:21:28 +0200

    tehtava 2.6. tehty

---
+++ osa2/tehtava2.6/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Person from './components/Person'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            persons: [
+                { name: 'Arto Hellas' }
+            ],
+            newName: ''
+        }
+    }
+
+    addName = (event) => {
+        event.preventDefault()
+        const nameObject = {
+            name: this.state.newName
+        }
+
+        const persons = this.state.persons.concat(nameObject)
+
+        this.setState({
+            persons: persons,
+            newNote: ''
+        })
+    }
+
+    handleNameChange = (event) => {
+        console.log(event.target.value)
+        this.setState({ newName: event.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Puhelinluettelo</h2>
+                <form onSubmit={this.addName}>
+                    <div>
+                        nimi:
+                        <input
+                            value={this.state.newName}
+                            onChange={this.handleNameChange}
+                        />
+                    </div>
+                    <div>
+                    </div>
+                    <button type="submit">lisää</button>
+                </form>
+                <h2>Numerot</h2>
+                <div>
+                    {this.state.persons.map(person => <Person key={person.name} person={person} />)}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default App
+
+

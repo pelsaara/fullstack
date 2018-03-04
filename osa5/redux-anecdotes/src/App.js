387c01fcd0cfa75d573f63ef89c7a18f8d5f9c22
pelsaara <saara.peltomaki@gmail.com>
2018-03-04 22:23:51 +0200

    Anekdootit tehty

---
+++ osa5/redux-anecdotes/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  voteAnecdote = (id) => () => {
+    this.props.store.dispatch({
+      type: 'VOTE',
+      data: { id }
+    })
+  }
+
+  render() {
+    const anecdotes = this.props.store.getState()
+    const sortedAnecdotes = anecdotes.sort((a, b) => a.votes - b.votes < 0)
+
+
+
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {sortedAnecdotes.map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={this.voteAnecdote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+        <h2>create new</h2>
+        <form>
+          <div><input /></div>
+          <button>create</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
